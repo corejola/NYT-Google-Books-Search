89e248edddc249cb7ea7b322599e0552edc67ebe
Cedric Orejola <cedricorejola@gmail.com>
2019-08-13 20:21:03 -0700

    working through the API routing

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,21 +1,50 @@
 import React, { Component } from "react";
-import Input from "../components/Input/index";
+import Input from "../components/Input";
+import Button from "../components/Button"
+import API from "../utils/API";
 
 class Search extends Component {
     state = {
-        books: []
+        books: [],
+        bookSearch: ""
     }
+
+    handleInputChange = event => {
+        // Destructure the name and value properties off of event.target
+        // Update the appropriate state
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+        event.preventDefault();
+        API.getRecipes(this.state.bookSearch)
+            .then(res => this.setState({ books: res.data }))
+            .catch(err => console.log(err));
+
+        console.log("BUTTON CLICKED")
+    };
 
     render() {
         return (
             <div>
                 <h1>TEST SEARCH PAGE</h1>
                 <Input
-                    name="recipeSearch"
-                    value={this.state.books}
+                    name="bookSearch"
+                    value={this.state.bookSearch}
                     onChange={this.handleInputChange}
                     placeholder="Search For a Book"
                 />
+                <Button
+                    onClick={this.handleFormSubmit}
+                    type="success"
+                    className="input-lg"
+                >
+                    Search
+                      </Button>
             </div>
         )
     }
